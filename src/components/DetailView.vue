37a223aa41c2a911818f9d05528439b45b593594
Rachana Prakash <rachana.prakash91@gmail.com>
2023-11-04 08:32:37 +0530

    linking fonts correctly

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -32,6 +32,7 @@ export default {
   background-color: white;
   border: solid 2px var(--steel-blue);
   border-radius: 20%;
+  box-shadow: 0 2px 20px rgba(18, 134, 191, 0.1);
 }
 .img-container {
   margin: 4px;
@@ -56,5 +57,6 @@ img {
   font-size: 16px;
   font-weight: 300;
   padding: 4px;
+  line-height: 28px;
 }
 </style>
